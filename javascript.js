314502b7f87ea899c4f4c4481584ebc5328462ea
rounakk001 <150698038+rounakk001@users.noreply.github.com>
2023-11-29 00:40:45 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+let food1s = document.getElementById('food1');
+let food2s = document.getElementById('food2');
+let foods = document.getElementById('food');
+
+food1s.addEventListener('click',()=>{
+    foods.style.backgroundImage="url('food1.png')";
+})
+
+food2s.addEventListener('click',()=>{
+    foods.style.backgroundImage="url('food.jpg')";
+})
+ 133 changes: 133 additions & 0 deletions133  
+style.css
